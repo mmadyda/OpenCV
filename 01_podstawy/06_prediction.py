d8a372734e17242c17cbe4cf991388bdb2b852ed
marek <marekmadyda@gmail.com>
2020-05-18 18:49:59 +0200

    kamaro

--- 01_podstawy/06_prediction.py
+++ 01_podstawy/06_prediction.py
@@ -10,9 +10,21 @@ model = tf.keras.models.load_model('techni.h5')
 
 while True:
     ret, frame = cap.read()
+    resized = np.array(cv2.resize(frame, (256, 256)), dtype='float32')
+    resized = resized/255.0
+    resized = np.expand_dims(resized,0)
 
-    pred = model.predict
+    #print(resized.shape)
+
+
+    pred = model.predict(resized)[0,0]
+    text = ''
     print(f'predict: {pred}')
+    if pred > 0.5:
+        text = f'Tlo: {pred}'
+    else:
+        text = f'Marek: {pred}'
+    cv2.putText(frame,text,(20,40),cv2.FONT_HERSHEY_SIMPLEX,fontScale=1,color=(0,0,255),thickness=4)
     cv2.imshow('predict', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
